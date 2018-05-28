40f4cfed5bfbc9dc5048755cb4a81bd0ef213cc4
robuntu <robuntu@robuntu.ca>
2018-05-28 09:48:19 -0400

    Commented out findByCoach because it seems useless

--- app/models/Team.java
+++ app/models/Team.java
@@ -50,7 +50,7 @@ public class Team {
         }
     }
 
-    public static List<Team> findByCoach(String email){
-
-    }
+//    public static List<Team> findByCoach(String email){
+//        
+//    }
 }
